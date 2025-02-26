80bd8b61bc5f4ebdde8ae5762aa34b968041f979
sorrow scavenger <hatim.touil0@gmail.com>
2025-02-26 21:46:47 +0000

    organizing the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./*.html", "./pages/**/*.html", "./src/**/*.{ts,js}"],
   theme: {
     extend: {
+      screens: {
+        xs: {
+          max: '639px',
+        },
+      },
       fontSize: {
         '2xs': ['0.625rem', {
           lineHeight: '1rem',
