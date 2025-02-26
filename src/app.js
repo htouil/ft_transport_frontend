2c65da69d80f66b3288949750fe9cfa4fd81efd9
sorrow scavenger <hatim.touil0@gmail.com>
2025-02-26 00:56:36 +0000

    minor fixes

--- src/app.js
+++ src/app.js
@@ -8,94 +8,181 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+let app;
+let navBar;
+let navBtn;
+let onlineBtn;
+let localBtn;
+let sidePanel;
+let openBtn;
+let closeBtn;
+let friendsBtn;
+let historyBtn;
+let friendsList;
+let historyList;
 document.addEventListener('DOMContentLoaded', () => {
-    const app = document.getElementById('app');
-    const navBtn = document.querySelectorAll('.nav-btn');
-    const navBar = document.querySelector('nav');
-    const loadPage = (page) => __awaiter(void 0, void 0, void 0, function* () {
-        try {
-            const response = yield fetch(`pages/${page}.html`);
-            const content = yield response.text();
-            app.innerHTML = content;
-            hideNav(page);
-            const openbtn = document.getElementById('open-btn');
-            const closebtn = document.getElementById('close-btn');
-            const side_panel = document.getElementById('friends-panel');
-            const frs_btn = document.getElementById('friends-btn');
-            const htr_btn = document.getElementById('history-btn');
-            const frs_list = document.getElementById('friends-list');
-            const htr_list = document.getElementById('history-list');
-            openbtn === null || openbtn === void 0 ? void 0 : openbtn.addEventListener('click', (e) => {
-                e.stopPropagation();
-                side_panel === null || side_panel === void 0 ? void 0 : side_panel.classList.remove('translate-x-full');
-                document.body.classList.add('overflow-hidden');
-                frs_list === null || frs_list === void 0 ? void 0 : frs_list.classList.remove('hidden');
-                htr_list === null || htr_list === void 0 ? void 0 : htr_list.classList.add('hidden');
-            });
-            closebtn === null || closebtn === void 0 ? void 0 : closebtn.addEventListener('click', (e) => {
-                e.stopPropagation();
-                side_panel === null || side_panel === void 0 ? void 0 : side_panel.classList.add('translate-x-full');
-                document.body.classList.remove('overflow-hidden');
-            });
-            document.addEventListener('click', (e) => {
-                if (side_panel && !side_panel.contains(e.target) && e.target !== openbtn) {
-                    side_panel === null || side_panel === void 0 ? void 0 : side_panel.classList.add('translate-x-full');
-                    document.body.classList.remove('overflow-hidden');
-                }
-            });
-            frs_btn === null || frs_btn === void 0 ? void 0 : frs_btn.addEventListener('click', () => {
-                frs_list === null || frs_list === void 0 ? void 0 : frs_list.classList.remove('hidden');
-                htr_list === null || htr_list === void 0 ? void 0 : htr_list.classList.add('hidden');
-            });
-            htr_btn === null || htr_btn === void 0 ? void 0 : htr_btn.addEventListener('click', () => {
-                htr_list === null || htr_list === void 0 ? void 0 : htr_list.classList.remove('hidden');
-                frs_list === null || frs_list === void 0 ? void 0 : frs_list.classList.add('hidden');
-            });
-        }
-        catch (error) {
-            app.innerHTML = `<p class="text-red-500">Page not found.</p>`;
-        }
-    });
-    const hideNav = (page) => {
-        if (navBar && page === 'profil') {
-            navBar.classList.remove('flex');
-            navBar.classList.add('hidden');
-        }
-    };
-    navBtn.forEach(button => {
+    app = document.getElementById('app');
+    navBar = document.querySelector('nav');
+    navBtn = document.querySelectorAll('.nav-btn');
+    loadPage('home');
+    navBtn.forEach((button) => {
         button.addEventListener('click', () => {
             const page = button.dataset.page;
-            console.log(`Navigating to: ${page}`);
+            // console.log(`Navigating to: ${page}`);
             loadPage(page);
         });
     });
-    loadPage('home');
 });
-// document.addEventListener('click', (e) =>
+const loadPage = (page) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield fetch(`pages/${page}.html`);
+        const content = yield response.text();
+        app.innerHTML = content;
+        hideNav(page);
+        setupButtons();
+    }
+    catch (error) {
+        app.innerHTML = `<p class="text-red-500">Page not found.</p>`;
+    }
+});
+const hideNav = (page) => {
+    if (navBar && page === 'profil') {
+        navBar.classList.remove('flex');
+        navBar.classList.add('hidden');
+    }
+};
+const setupButtons = () => {
+    localBtn = document.getElementById('local-btn');
+    onlineBtn = document.getElementById('online-btn');
+    sidePanel = document.getElementById('friends-panel');
+    openBtn = document.getElementById('open-btn');
+    closeBtn = document.getElementById('close-btn');
+    friendsBtn = document.getElementById('friends-btn');
+    historyBtn = document.getElementById('history-btn');
+    friendsList = document.getElementById('friends-list');
+    historyList = document.getElementById('history-list');
+    localBtn === null || localBtn === void 0 ? void 0 : localBtn.addEventListener('click', selectLocal);
+    onlineBtn === null || onlineBtn === void 0 ? void 0 : onlineBtn.addEventListener('click', selectOnline);
+    openBtn === null || openBtn === void 0 ? void 0 : openBtn.addEventListener('click', openSidePanel);
+    closeBtn === null || closeBtn === void 0 ? void 0 : closeBtn.addEventListener('click', closeSidePanel);
+    document.addEventListener('click', handleOutsideClick);
+    friendsBtn === null || friendsBtn === void 0 ? void 0 : friendsBtn.addEventListener('click', showFriendsList);
+    historyBtn === null || historyBtn === void 0 ? void 0 : historyBtn.addEventListener('click', showHistoryList);
+};
+const selectLocal = () => {
+    localBtn === null || localBtn === void 0 ? void 0 : localBtn.classList.add('game-btn');
+    onlineBtn === null || onlineBtn === void 0 ? void 0 : onlineBtn.classList.remove('game-btn');
+};
+//remove focus when game mode button selected
+const selectOnline = () => {
+    onlineBtn === null || onlineBtn === void 0 ? void 0 : onlineBtn.classList.add('game-btn');
+    localBtn === null || localBtn === void 0 ? void 0 : localBtn.classList.remove('game-btn');
+};
+const openSidePanel = (e) => {
+    e.stopPropagation();
+    sidePanel === null || sidePanel === void 0 ? void 0 : sidePanel.classList.remove('translate-x-full');
+    document.body.classList.add('overflow-hidden');
+    showFriendsList();
+};
+const closeSidePanel = (e) => {
+    e === null || e === void 0 ? void 0 : e.stopPropagation();
+    sidePanel === null || sidePanel === void 0 ? void 0 : sidePanel.classList.add('translate-x-full');
+    document.body.classList.remove('overflow-hidden');
+};
+const handleOutsideClick = (e) => {
+    if (sidePanel && !sidePanel.contains(e.target) && e.target !== openBtn)
+        closeSidePanel();
+};
+const showFriendsList = () => {
+    friendsList === null || friendsList === void 0 ? void 0 : friendsList.classList.remove('hidden');
+    historyList === null || historyList === void 0 ? void 0 : historyList.classList.add('hidden');
+    friendsBtn === null || friendsBtn === void 0 ? void 0 : friendsBtn.classList.remove('panel-btn');
+    friendsBtn === null || friendsBtn === void 0 ? void 0 : friendsBtn.classList.add('selected-panel-btn');
+    historyBtn === null || historyBtn === void 0 ? void 0 : historyBtn.classList.add('panel-btn');
+    historyBtn === null || historyBtn === void 0 ? void 0 : historyBtn.classList.remove('selected-panel-btn');
+};
+const showHistoryList = () => {
+    historyList === null || historyList === void 0 ? void 0 : historyList.classList.remove('hidden');
+    friendsList === null || friendsList === void 0 ? void 0 : friendsList.classList.add('hidden');
+    historyBtn === null || historyBtn === void 0 ? void 0 : historyBtn.classList.remove('panel-btn');
+    historyBtn === null || historyBtn === void 0 ? void 0 : historyBtn.classList.add('selected-panel-btn');
+    friendsBtn === null || friendsBtn === void 0 ? void 0 : friendsBtn.classList.add('panel-btn');
+    friendsBtn === null || friendsBtn === void 0 ? void 0 : friendsBtn.classList.remove('selected-panel-btn');
+};
+// document.addEventListener('DOMContentLoaded', () =>
 // {
-// 	const otherbtn = (e.target as HTMLElement).closest('.other-btn');
-// 	const activeButton = document.querySelector('.other-btn:focus, .other-btn:hover, .other-btn:active') as HTMLElement | null;
-// 	if (panel && !panel.contains(e.target as Node))
+// 	const app = document.getElementById('app')!;
+// 	const navBtn = document.querySelectorAll('.nav-btn');
+// 	const navBar = document.querySelector('nav');
+// 	const loadPage = async (page: string) =>
 // 	{
-// 		if (!otherbtn)
-// 			panel.classList.add('translate-x-full');
-// 		else
-// 		{
-// 			if (!(panel?.classList.contains('translate-x-full')))
+// 		try {
+// 			const response = await fetch(`pages/${page}.html`);
+// 			const content = await response.text();
+// 			app.innerHTML = content;
+// 			hideNav(page);
+// 			const openbtn = document.getElementById('open-btn');
+// 			const closebtn = document.getElementById('close-btn');
+// 			const side_panel = document.getElementById('friends-panel');
+// 			const frs_btn = document.getElementById('friends-btn');
+// 			const htr_btn = document.getElementById('history-btn');
+// 			const frs_list = document.getElementById('friends-list');
+// 			const htr_list = document.getElementById('history-list');
+// 			openbtn?.addEventListener('click', (e) =>
 // 			{
-// 				e.preventDefault();
-// 			}
-// 			panel?.classList.add('translate-x-full');
-// 		}
-// 		if (activeButton)
+// 				e.stopPropagation();
+// 				side_panel?.classList.remove('translate-x-full');
+// 				document.body.classList.add('overflow-hidden');
+// 				frs_list?.classList.remove('hidden');
+// 				htr_list?.classList.add('hidden');
+// 			});
+// 			closebtn?.addEventListener('click', (e) =>
+// 			{
+// 				e.stopPropagation();
+// 				side_panel?.classList.add('translate-x-full');
+// 				document.body.classList.remove('overflow-hidden');
+// 			});
+// 			document.addEventListener('click', (e) =>
+// 			{
+// 				if (side_panel && !side_panel.contains(e.target as Node) && e.target !== openbtn)
+// 				{
+// 					side_panel?.classList.add('translate-x-full');
+// 					document.body.classList.remove('overflow-hidden');
+// 				}
+// 			});
+// 			frs_btn?.addEventListener('click', () =>
+// 			{
+// 				frs_list?.classList.remove('hidden');
+// 				htr_list?.classList.add('hidden');
+// 			});
+// 			htr_btn?.addEventListener('click', () =>
+// 			{
+// 				htr_list?.classList.remove('hidden');
+// 				frs_list?.classList.add('hidden');
+// 			});
+// 		}	
+// 		catch (error)
 // 		{
-// 			activeButton.blur();
-// 			activeButton.style.pointerEvents = 'none';
-// 			setTimeout(() => {
-// 				activeButton.style.pointerEvents = '';
-// 			}, 10);
-// 			// activeButton.classList.remove('hover', 'focus', 'active');
+// 			app.innerHTML = `<p class="text-red-500">Page not found.</p>`;
 // 		}
-// 	}
+// 	};	
+// 	const hideNav = (page: string) =>
+// 	{
+// 		if (navBar && page === 'profil')
+// 		{
+// 			navBar.classList.remove('flex');
+// 			navBar.classList.add('hidden');
+// 		}
+// 	};
+// 	navBtn.forEach((button) =>
+// 	{
+// 		button.addEventListener('click', () =>
+// 		{
+// 			const page = (button as HTMLElement).dataset.page!;
+// 			console.log(`Navigating to: ${page}`);
+// 			loadPage(page);
+// 		});
+// 	});
+// 	loadPage('home');
 // });
 // history.replaceState()
--- src/app.ts
+++ src/app.ts
@@ -1,116 +1,206 @@
-document.addEventListener('DOMContentLoaded', () =>
-{
-	const app = document.getElementById('app')!;
-	const navBtn = document.querySelectorAll('.nav-btn');
-	const navBar = document.querySelector('nav');
+let app: HTMLElement;
+let navBar: HTMLElement | null;
+let navBtn: NodeListOf<HTMLElement> | null;
+let onlineBtn: HTMLElement | null;
+let localBtn: HTMLElement | null;
+let sidePanel: HTMLElement | null;
+let openBtn: HTMLElement | null;
+let closeBtn: HTMLElement | null;
+let friendsBtn: HTMLElement | null;
+let historyBtn: HTMLElement | null;
+let friendsList: HTMLElement | null;
+let historyList: HTMLElement | null;
+
+document.addEventListener('DOMContentLoaded', () => {
+	app = document.getElementById('app')!;
+	navBar = document.querySelector('nav');
+	navBtn = document.querySelectorAll('.nav-btn') as NodeListOf<HTMLElement>;
 	
-	const loadPage = async (page: string) =>
-	{
-		try {
-			const response = await fetch(`pages/${page}.html`);
-			const content = await response.text();
-			app.innerHTML = content;
-			hideNav(page);
-			const openbtn = document.getElementById('open-btn');
-			const closebtn = document.getElementById('close-btn');
-			const side_panel = document.getElementById('friends-panel');
-			const frs_btn = document.getElementById('friends-btn');
-			const htr_btn = document.getElementById('history-btn');
-			const frs_list = document.getElementById('friends-list');
-			const htr_list = document.getElementById('history-list');
-
-			openbtn?.addEventListener('click', (e) =>
-			{
-				e.stopPropagation();
-				side_panel?.classList.remove('translate-x-full');
-				document.body.classList.add('overflow-hidden');
-
-				frs_list?.classList.remove('hidden');
-				htr_list?.classList.add('hidden');
-			});
-
-			closebtn?.addEventListener('click', (e) =>
-			{
-				e.stopPropagation();
-				side_panel?.classList.add('translate-x-full');
-				document.body.classList.remove('overflow-hidden');
-			});
-
-			document.addEventListener('click', (e) =>
-			{
-				if (side_panel && !side_panel.contains(e.target as Node) && e.target !== openbtn)
-				{
-					side_panel?.classList.add('translate-x-full');
-					document.body.classList.remove('overflow-hidden');
-				}
-			});
-
-			frs_btn?.addEventListener('click', () =>
-			{
-				frs_list?.classList.remove('hidden');
-				htr_list?.classList.add('hidden');
-			});
-			
-			htr_btn?.addEventListener('click', () =>
-			{
-				htr_list?.classList.remove('hidden');
-				frs_list?.classList.add('hidden');
-			});
-		}	
-		catch (error)
-		{
-			app.innerHTML = `<p class="text-red-500">Page not found.</p>`;
-		}	
-	};	
-	
-	const hideNav = (page: string) =>
-	{
-		if (navBar && page === 'profil')
-		{
-			navBar.classList.remove('flex');
-			navBar.classList.add('hidden');
-		}	
-	};	
-	
-	navBtn.forEach(button =>
+	loadPage('home');
+	navBtn.forEach((button) =>
 	{
 		button.addEventListener('click', () =>
 		{
 			const page = (button as HTMLElement).dataset.page!;
-			console.log(`Navigating to: ${page}`);
+			// console.log(`Navigating to: ${page}`);
 			loadPage(page);
-		});	
+		});
 	});
-	loadPage('home');
 });
+
+const loadPage = async (page: string) => {
+	try {
+		const response = await fetch(`pages/${page}.html`);
+		const content = await response.text();
+		app.innerHTML = content;
+		hideNav(page);
+		setupButtons();
+	}
+	catch (error)
+	{
+		app.innerHTML = `<p class="text-red-500">Page not found.</p>`;
+	}
+};
+
+const hideNav = (page: string) =>
+{
+	if (navBar && page === 'profil')
+	{
+		navBar.classList.remove('flex');
+		navBar.classList.add('hidden');
+	}
+};
+
+const setupButtons = () => {
+	localBtn = document.getElementById('local-btn');
+	onlineBtn = document.getElementById('online-btn');
+	sidePanel = document.getElementById('friends-panel');
+	openBtn = document.getElementById('open-btn');
+	closeBtn = document.getElementById('close-btn');
+	friendsBtn = document.getElementById('friends-btn');
+	historyBtn = document.getElementById('history-btn');
+	friendsList = document.getElementById('friends-list');
+	historyList = document.getElementById('history-list');
+
+	localBtn?.addEventListener('click', selectLocal);
+	onlineBtn?.addEventListener('click', selectOnline);
+	openBtn?.addEventListener('click', openSidePanel);
+	closeBtn?.addEventListener('click', closeSidePanel);
+	document.addEventListener('click', handleOutsideClick);
+	friendsBtn?.addEventListener('click', showFriendsList);
+	historyBtn?.addEventListener('click', showHistoryList);
+};
+
+const selectLocal = () => {
+	localBtn?.classList.add('game-btn');
+	onlineBtn?.classList.remove('game-btn');
+};
+//remove focus when game mode button selected
+const selectOnline = () => {
+	onlineBtn?.classList.add('game-btn');
+	localBtn?.classList.remove('game-btn');
+};
+
+const openSidePanel = (e: Event) => {
+	e.stopPropagation();
+	sidePanel?.classList.remove('translate-x-full');
+	document.body.classList.add('overflow-hidden');
+	showFriendsList();
+};
+
+const closeSidePanel = (e?: Event) => {
+	e?.stopPropagation();
+	sidePanel?.classList.add('translate-x-full');
+	document.body.classList.remove('overflow-hidden');
+};
+
+const handleOutsideClick = (e: Event) => {
+	if (sidePanel && !sidePanel.contains(e.target as Node) && e.target !== openBtn)
+	closeSidePanel();
+};
+
+const showFriendsList = () => {
+	friendsList?.classList.remove('hidden');
+	historyList?.classList.add('hidden');
+	friendsBtn?.classList.remove('panel-btn');
+	friendsBtn?.classList.add('selected-panel-btn');
+	historyBtn?.classList.add('panel-btn');
+	historyBtn?.classList.remove('selected-panel-btn');
+};
+
+const showHistoryList = () => {
+	historyList?.classList.remove('hidden');
+	friendsList?.classList.add('hidden');
+	historyBtn?.classList.remove('panel-btn');
+	historyBtn?.classList.add('selected-panel-btn');
+	friendsBtn?.classList.add('panel-btn');
+	friendsBtn?.classList.remove('selected-panel-btn');
+}
+
+// document.addEventListener('DOMContentLoaded', () =>
+// {
+// 	const app = document.getElementById('app')!;
+// 	const navBtn = document.querySelectorAll('.nav-btn');
+// 	const navBar = document.querySelector('nav');
 	
-	// document.addEventListener('click', (e) =>
-	// {
-	// 	const otherbtn = (e.target as HTMLElement).closest('.other-btn');
-	// 	const activeButton = document.querySelector('.other-btn:focus, .other-btn:hover, .other-btn:active') as HTMLElement | null;
+// 	const loadPage = async (page: string) =>
+// 	{
+// 		try {
+// 			const response = await fetch(`pages/${page}.html`);
+// 			const content = await response.text();
+// 			app.innerHTML = content;
+// 			hideNav(page);
+// 			const openbtn = document.getElementById('open-btn');
+// 			const closebtn = document.getElementById('close-btn');
+// 			const side_panel = document.getElementById('friends-panel');
+// 			const frs_btn = document.getElementById('friends-btn');
+// 			const htr_btn = document.getElementById('history-btn');
+// 			const frs_list = document.getElementById('friends-list');
+// 			const htr_list = document.getElementById('history-list');
 
-	// 	if (panel && !panel.contains(e.target as Node))
-	// 	{
-	// 		if (!otherbtn)
-	// 			panel.classList.add('translate-x-full');
-	// 		else
-	// 		{
-	// 			if (!(panel?.classList.contains('translate-x-full')))
-	// 			{
-	// 				e.preventDefault();
-	// 			}
-	// 			panel?.classList.add('translate-x-full');
-	// 		}
-	// 		if (activeButton)
-	// 		{
-	// 			activeButton.blur();
-	// 			activeButton.style.pointerEvents = 'none';
-	// 			setTimeout(() => {
-	// 				activeButton.style.pointerEvents = '';
-	// 			}, 10);
-	// 			// activeButton.classList.remove('hover', 'focus', 'active');
-	// 		}
-	// 	}
-	// });
+// 			openbtn?.addEventListener('click', (e) =>
+// 			{
+// 				e.stopPropagation();
+// 				side_panel?.classList.remove('translate-x-full');
+// 				document.body.classList.add('overflow-hidden');
 
-	// history.replaceState()
+// 				frs_list?.classList.remove('hidden');
+// 				htr_list?.classList.add('hidden');
+// 			});
+
+// 			closebtn?.addEventListener('click', (e) =>
+// 			{
+// 				e.stopPropagation();
+// 				side_panel?.classList.add('translate-x-full');
+// 				document.body.classList.remove('overflow-hidden');
+// 			});
+
+// 			document.addEventListener('click', (e) =>
+// 			{
+// 				if (side_panel && !side_panel.contains(e.target as Node) && e.target !== openbtn)
+// 				{
+// 					side_panel?.classList.add('translate-x-full');
+// 					document.body.classList.remove('overflow-hidden');
+// 				}
+// 			});
+
+// 			frs_btn?.addEventListener('click', () =>
+// 			{
+// 				frs_list?.classList.remove('hidden');
+// 				htr_list?.classList.add('hidden');
+// 			});
+			
+// 			htr_btn?.addEventListener('click', () =>
+// 			{
+// 				htr_list?.classList.remove('hidden');
+// 				frs_list?.classList.add('hidden');
+// 			});
+// 		}	
+// 		catch (error)
+// 		{
+// 			app.innerHTML = `<p class="text-red-500">Page not found.</p>`;
+// 		}
+// 	};	
+	
+// 	const hideNav = (page: string) =>
+// 	{
+// 		if (navBar && page === 'profil')
+// 		{
+// 			navBar.classList.remove('flex');
+// 			navBar.classList.add('hidden');
+// 		}
+// 	};
+
+// 	navBtn.forEach((button) =>
+// 	{
+// 		button.addEventListener('click', () =>
+// 		{
+// 			const page = (button as HTMLElement).dataset.page!;
+// 			console.log(`Navigating to: ${page}`);
+// 			loadPage(page);
+// 		});
+// 	});
+// 	loadPage('home');
+// });
+
+// history.replaceState()
